fcd8d458a709a2802cca4bd9ffdf2eae0a1809bb
Rashad Nelson <rashadnelson@gmail.com>
2022-02-13 12:58:59 -0500

    CodeWars - updated function - 02/13/22

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const unrepeated = (str) => [...new Set(str)].join('');
+// Write a simple function to iterate through the array and console.log to test the results.
 
-console.log(unrepeated("hello"))
+// Place each array element in new Set() function.  Assign this to a variable.  Console.log the variable.  
+
+function unreapeatedArray(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        // console.log(arr[i])
+
+        let unrepeated = new Set(arr[i])
+
+        console.log(unrepeated)
+    }
+}
+unreapeatedArray(["1223", "334455", "667788", "7798956443211"])
+
+
